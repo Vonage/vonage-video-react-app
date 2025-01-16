0578a075c2b0688a102f41741b55921aec8071d6
Mike Thomas <mike.george.thomas@gmail.com>
2025-01-16 17:40:58 +0100

    TDD: Add failing test for current race condition

---
+++ backend/tests/getOrCreateSession.test.ts
@@ -0,0 +1,81 @@
+import { describe, expect, it, jest } from '@jest/globals';
+import { Archive } from 'opentok';
+
+await jest.unstable_mockModule('../helpers/config', () => {
+  return {
+    default: jest.fn().mockImplementation(() => {
+      return {
+        apiKey: 'test-api-key',
+        apiSecret: 'test-api-secret',
+        applicationId: 'test-application-id',
+        privateKey: 'test-private-key',
+        provider: 'opentok',
+      };
+    }),
+  };
+});
+
+const mockVcrSessionStorage = {
+  getSession: jest.fn().mockReturnValue('someSessionId'),
+  setSession: jest.fn().mockReturnValue(true),
+};
+
+jest.mock('../storage/vcrSessionStorage', () => {
+  return jest.fn().mockImplementation(() => mockVcrSessionStorage);
+});
+
+const mockCredentials = {
+  sessionId: 'session1',
+  token: 'someToken',
+  apiKey: 'someApiKey',
+};
+const getCredentialsMock = jest
+  .fn<() => Promise<{ sessionId: string; token: string; apiKey: string }>>()
+  .mockResolvedValue(mockCredentials);
+
+await jest.unstable_mockModule('../opentok', () => {
+  return {
+    default: jest.fn().mockImplementation(() => {
+      return {
+        startArchive: jest.fn<() => Promise<string>>().mockResolvedValue('archiveId'),
+        stopArchive: jest.fn<() => Promise<string>>().mockRejectedValue('invalid archive'),
+        generateToken: jest
+          .fn<() => Promise<{ token: string; apiKey: string }>>()
+          .mockResolvedValue({
+            token: 'someToken',
+            apiKey: 'someApiKey',
+          }),
+        createSession: jest.fn(),
+        listArchives: jest
+          .fn<() => Promise<Archive[]>>()
+          .mockResolvedValue([{ id: 'archive1' }, { id: 'archive2' }] as unknown as Archive[]),
+        getCredentials: getCredentialsMock,
+      };
+    }),
+  };
+});
+
+const { getOrCreateSession } = await import('../routes/session');
+
+// TODO: cleanup this file and simplify mocks / imports
+describe('getOrCreateSession', () => {
+  it('should only create one session for simultaneous requests', async () => {
+    mockVcrSessionStorage.getSession.mockReturnValue('');
+    getCredentialsMock.mockResolvedValueOnce({
+      sessionId: 'session1',
+      token: 'someToken',
+      apiKey: 'someApiKey',
+    });
+    getCredentialsMock.mockResolvedValueOnce({
+      sessionId: 'session2',
+      token: 'someToken',
+      apiKey: 'someApiKey',
+    });
+    const request1 = getOrCreateSession('my-new-room');
+    const request2 = getOrCreateSession('my-new-room');
+    const response1 = await request1;
+    const response2 = await request2;
+    expect(response1.sessionId).toEqual('session1');
+    expect(response2.sessionId).toEqual('session1');
+  });
+});
