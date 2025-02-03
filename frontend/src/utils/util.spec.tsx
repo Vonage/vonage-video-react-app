80d046051b4dd9eb10fee6cdfeff7ed0a23cd7f5
Christian Pettet <71297280+cpettet@users.noreply.github.com>
2025-02-03 13:25:05 -0500

    5 and 5

--- frontend/src/utils/util.spec.tsx
+++ frontend/src/utils/util.spec.tsx
@@ -113,11 +113,13 @@ describe('getAudioSourceDeviceId', () => {
 });
 
 describe('isMobile', () => {
-  it('should return false for Microsoft tablets', () => {
+  it('should return false for non-mobile devices', () => {
     const tabletUAStrings = [
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0',
       'Mozilla/5.0 (Windows NT 10.0; ARM64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+      'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
     ];
 
     tabletUAStrings.forEach((tabletUAString) => {
@@ -133,7 +135,6 @@ describe('isMobile', () => {
 
   it('should return true for mobile devices', () => {
     const mobileUAStrings = [
-      'Mozilla/5.0 (X11; U; Linux armv7l; en-GB; rv:1.9.2a1pre) Gecko/20090928 Firefox/3.5 Maemo Browser 1.4.1.22 RX-51 N900',
       'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
       'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36',
       'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/131.0.0.0 Mobile/15E148 Safari/604.1',
