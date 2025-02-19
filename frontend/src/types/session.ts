79b3b812e2fe0d1d4b2da9d6af7e8cbda2c7b656
Mike Thomas <mike.george.thomas@gmail.com>
2025-02-19 16:49:12 +0100

    more comments

--- frontend/src/types/session.ts
+++ frontend/src/types/session.ts
@@ -1,7 +1,7 @@
 import { Subscriber } from '@vonage/client-sdk-video';
 
 /**
- * Wrapper for a subscriber, including the DOM element, the subscriber object, and its screen sharing status.
+ * Wrapper for a subscriber, including the DOM element, the subscriber object, whether it's a screenshare subscriber and whether it has been pinned.
  */
 export type SubscriberWrapper = {
   element: HTMLVideoElement | HTMLObjectElement;
