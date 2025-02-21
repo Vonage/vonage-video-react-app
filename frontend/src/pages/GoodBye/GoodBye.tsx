7b9933a1daf16c70c3d26515982c7b440ff0f20a
Misha Behei <misha.behei@vonage.com>
2025-02-20 19:03:55 -0600

    latest lint fix

--- frontend/src/pages/GoodBye/GoodBye.tsx
+++ frontend/src/pages/GoodBye/GoodBye.tsx
@@ -39,8 +39,8 @@ const GoodBye = (): ReactElement => {
           <div className="max-w-[400px]">
             <GoodByeMessage header={header} message={caption} roomName={roomName} />
           </div>
-          <div className="ps-12 py-4 h-auto shrink w-full text-left">
-            <h3 className="text-4xl text-black pb-5 w-9/12">Recordings</h3>
+          <div className="h-auto w-full shrink py-4 ps-12 text-left">
+            <h3 className="w-9/12 pb-5 text-4xl text-black">Recordings</h3>
             <ArchiveList archives={archives} />
           </div>
         </div>
