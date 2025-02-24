ab38a4d4dd5ce2df1ebbd80a11a50d9dc36f1448
Mike Thomas <mike.george.thomas@gmail.com>
2025-02-24 16:36:44 +0000

    jsdoc

--- frontend/src/utils/sessionStateOperations/togglePinAndSortByDisplayOrder.ts
+++ frontend/src/utils/sessionStateOperations/togglePinAndSortByDisplayOrder.ts
@@ -1,6 +1,16 @@
 import { SubscriberWrapper } from '../../types/session';
 import sortByDisplayPriority from './sortByDisplayPriority';
 
+/**
+ * togglePinAndSortByDisplayOrder - a util to operate on React State array of subscriberWrappers.
+ * This util will toggle the pinned state for the subscriber with the id passed in,
+ * and also re-order the array to ensure pinned subscribers are above regular the rest.
+ * (See ./sortByDisplayPriority for more details).
+ * @param {string} id - id of subscriber to pin / unpin.
+ * @param {SubscriberWrapper[]} previousSubscriberWrappers - previous array, obtained from setState function callback
+ * @param {(string | undefined)} activeSpeakerId - current activeSpeakerId
+ * @returns {SubscriberWrapper[]} - updated copy of subscriberWrapper array (React state is read-only so a copy is made).
+ */
 const togglePinAndSortByDisplayOrder = (
   id: string,
   previousSubscriberWrappers: SubscriberWrapper[],
