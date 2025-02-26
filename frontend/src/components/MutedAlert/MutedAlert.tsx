ac437859ed4022122abd39d35c299c1e9fbc202f
Christian Pettet <71297280+cpettet@users.noreply.github.com>
2025-02-26 17:55:51 -0500

    wip

--- frontend/src/components/MutedAlert/MutedAlert.tsx
+++ frontend/src/components/MutedAlert/MutedAlert.tsx
@@ -5,6 +5,7 @@ import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import { useTheme } from '@mui/material/styles';
 import { useState, useEffect, RefObject, ReactElement } from 'react';
+import { Alert, Box } from '@mui/material';
 import { MUTED_ALERT_MESSAGE, FORCE_MUTED_ALERT_MESSAGE } from '../../utils/constants';
 import useSpeakingDetector from '../../hooks/useSpeakingDetector';
 import usePublisherContext from '../../hooks/usePublisherContext';
@@ -23,54 +24,74 @@ export type MutedAlertProps = {
  */
 const MutedAlert = ({ anchorRef }: MutedAlertProps): ReactElement => {
   const { publisher, isAudioEnabled, isForceMuted } = usePublisherContext();
-  const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(true);
   const isSpeakingWhileMuted = useSpeakingDetector({
     isAudioEnabled,
     selectedMicrophoneId: publisher?.getAudioSource()?.id,
   });
   const theme = useTheme();
 
-  useEffect(() => {
-    setOpen(isForceMuted || isSpeakingWhileMuted);
-  }, [isForceMuted, isSpeakingWhileMuted]);
+  // useEffect(() => {
+  //   setOpen(isForceMuted || isSpeakingWhileMuted);
+  // }, [isForceMuted, isSpeakingWhileMuted]);
 
   return (
-    <Popper open={open} anchorEl={anchorRef.current} placement="top" transition>
-      {({ TransitionProps }) => (
-        <Fade {...TransitionProps} timeout={350}>
-          <Paper
-            sx={{
-              p: 2,
-              backgroundColor: 'rgb(60, 64, 67)',
-              color: '#fff',
-              maxWidth: 300,
-              width: 'auto',
-              borderRadius: 2,
-              boxShadow: 3,
-              fontSize: '0.875rem',
-              transform: 'translateY(-10%) translateX(-40%)',
-              // this is needed to center align on small devices
-              [theme.breakpoints.down(700)]: {
-                transform: 'translateY(-10%) translateX(25%)',
-              },
-            }}
-          >
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-              {isForceMuted && <span>{FORCE_MUTED_ALERT_MESSAGE}</span>}
-              {isSpeakingWhileMuted && !isForceMuted && <span>{MUTED_ALERT_MESSAGE}</span>}
-              <IconButton
-                onClick={() => setOpen(false)}
-                sx={{ color: '#fff', padding: 0, marginLeft: 1 }}
-                size="small"
-              >
-                <CloseIcon fontSize="small" />
-              </IconButton>
-            </div>
-          </Paper>
-        </Fade>
-      )}
-    </Popper>
+    <Alert
+      severity="warning"
+      sx={{
+        position: 'absolute',
+        maxWidth: '300px',
+        bottom: '96px',
+      }}
+    >
+      <span>{MUTED_ALERT_MESSAGE}</span>
+      <IconButton
+        onClick={() => setOpen(false)}
+        sx={{ color: '#fff', padding: 0, marginLeft: 1 }}
+        size="small"
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </Alert>
   );
+
+  // return (
+  //   <Popper open={open} anchorEl={anchorRef.current} placement="top" transition>
+  //     {({ TransitionProps }) => (
+  //       <Fade {...TransitionProps} timeout={350}>
+  //         <Paper
+  //           sx={{
+  //             p: 2,
+  //             backgroundColor: 'rgb(60, 64, 67)',
+  //             color: '#fff',
+  //             maxWidth: 300,
+  //             width: 'auto',
+  //             borderRadius: 2,
+  //             boxShadow: 3,
+  //             fontSize: '0.875rem',
+  //             transform: 'translateY(-10%) translateX(-40%)',
+  //             // this is needed to center align on small devices
+  //             [theme.breakpoints.down(700)]: {
+  //               transform: 'translateY(-10%) translateX(25%)',
+  //             },
+  //           }}
+  //         >
+  //           <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+  //             {/* {isForceMuted && <span>{FORCE_MUTED_ALERT_MESSAGE}</span>} */}
+  //             <span>{MUTED_ALERT_MESSAGE}</span>
+  //             <IconButton
+  //               onClick={() => setOpen(false)}
+  //               sx={{ color: '#fff', padding: 0, marginLeft: 1 }}
+  //               size="small"
+  //             >
+  //               <CloseIcon fontSize="small" />
+  //             </IconButton>
+  //           </div>
+  //         </Paper>
+  //       </Fade>
+  //     )}
+  //   </Popper>
+  // );
 };
 
 export default MutedAlert;
