88cb3f12a845f00abd62f98f800257ff1279c975
Christian Pettet <71297280+cpettet@users.noreply.github.com>
2025-03-04 17:13:12 -0500

    one file down

--- frontend/src/components/MeetingRoom/ToolbarOverflowMenu/ToolbarOverflowMenu.spec.tsx
+++ frontend/src/components/MeetingRoom/ToolbarOverflowMenu/ToolbarOverflowMenu.spec.tsx
@@ -1,17 +1,23 @@
-import { describe, expect, it, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, Mock, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { useRef } from 'react';
 import { Button } from '@mui/material';
 import ToolbarOverflowMenu from './ToolbarOverflowMenu';
+import * as util from '../../../utils/util';
+import useShownButtons from '../../../hooks/useShownButtons';
 
-const mockOpenEmojiGrid = vi.fn();
-const mockHandleClickAway = vi.fn();
 vi.mock('../../../hooks/useSessionContext', () => ({
   default: () => ({
     subscriberWrappers: [],
   }),
 }));
 vi.mock('../../../hooks/useRoomName');
+vi.mock('../../../utils/util', () => ({ isMobile: vi.fn() }));
+vi.mock('../../../hooks/useShownButtons');
+
+const mockOpenEmojiGrid = vi.fn();
+const mockHandleClickAway = vi.fn();
+const mockUseShownButtons = useShownButtons as Mock<[], number>;
 
 const TestComponent = ({ defaultOpen = false }: { defaultOpen?: boolean }) => {
   const anchorRef = useRef<HTMLButtonElement | null>(null);
@@ -23,14 +29,24 @@ const TestComponent = ({ defaultOpen = false }: { defaultOpen?: boolean }) => {
         isToolbarOverflowMenuOpen={defaultOpen}
         isEmojiGridOpen
         setIsEmojiGridOpen={mockOpenEmojiGrid}
-        anchorRef={anchorRef}
         handleClickAway={mockHandleClickAway}
+        toggleShareScreen={vi.fn()}
+        isSharingScreen={false}
       />
     </>
   );
 };
 
 describe('ToolbarOverflowMenu', () => {
+  beforeEach(() => {
+    (util.isMobile as Mock).mockImplementation(() => false);
+    mockUseShownButtons.mockReturnValue(0);
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
   it('is shown when open', () => {
     render(<TestComponent defaultOpen />);
 
@@ -40,12 +56,13 @@ describe('ToolbarOverflowMenu', () => {
   it('is not shown when closed', () => {
     render(<TestComponent />);
 
-    expect(screen.queryByTestId('toolbar-overflow-menu')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('toolbar-overflow-menu')).not.toBeVisible();
   });
 
   it('renders all the available buttons including the Report Issue button if enabled', () => {
     vi.stubEnv('VITE_ENABLE_REPORT_ISSUE', 'true');
     render(<TestComponent defaultOpen />);
+    expect(screen.getByTestId('screensharing-button')).toBeVisible();
     expect(screen.getByTestId('layout-button')).toBeVisible();
     expect(screen.getByTestId('archiving-button')).toBeVisible();
     expect(screen.getByTestId('emoji-grid-button')).toBeVisible();
@@ -57,11 +74,27 @@ describe('ToolbarOverflowMenu', () => {
   it('does not render Report Issue button in overflow menu if it was disabled', () => {
     vi.stubEnv('VITE_ENABLE_REPORT_ISSUE', 'false');
     render(<TestComponent defaultOpen />);
+    expect(screen.getByTestId('screensharing-button')).toBeVisible();
     expect(screen.getByTestId('layout-button')).toBeVisible();
     expect(screen.getByTestId('archiving-button')).toBeVisible();
     expect(screen.getByTestId('emoji-grid-button')).toBeVisible();
     expect(screen.queryByTestId('report-issue-button')).not.toBeInTheDocument();
     expect(screen.getByTestId('participant-list-button')).toBeVisible();
     expect(screen.getByTestId('chat-button')).toBeVisible();
+  });
+
+  describe('ScreenSharingButton', () => {
+    it('is not rendered for mobile devices', () => {
+      (util.isMobile as Mock).mockImplementation(() => true);
+      render(<TestComponent defaultOpen />);
+
+      expect(screen.queryByTestId('screensharing-button')).not.toBeInTheDocument();
+    });
+
+    it('is rendered for desktop devices', () => {
+      render(<TestComponent defaultOpen />);
+
+      expect(screen.getByTestId('screensharing-button')).toBeVisible();
+    });
   });
 });
