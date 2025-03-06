3bee52f90c624cd917e92c69c937aa081deaf0b8
Misha Behei <misha.behei@vonage.com>
2025-03-06 12:25:04 -0600

    a few more tests

--- frontend/src/components/MeetingRoom/ReportIssueButton/ReportIssueButton.spec.tsx
+++ frontend/src/components/MeetingRoom/ReportIssueButton/ReportIssueButton.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe, expect, it, vi } from 'vitest';
 import ReportIssueButton from './ReportIssueButton';
 
@@ -16,5 +17,19 @@ describe('ReportIssueButton', () => {
     render(<ReportIssueButton handleClick={handleClick} isOpen />);
     screen.getByRole('button').click();
     expect(handleClick).toHaveBeenCalled();
+  });
+  it('should have a tooltip title that indicates that chat can be opened', async () => {
+    render(<ReportIssueButton handleClick={() => {}} isOpen={false} />);
+    const button = await screen.findByRole('button');
+    await userEvent.hover(button);
+    const tooltip = await screen.findByRole('tooltip');
+    expect(tooltip).toHaveTextContent('Open report issue form');
+  });
+  it('should have a tooltip title that indicates that chat can be closed', async () => {
+    render(<ReportIssueButton handleClick={() => {}} isOpen />);
+    const button = await screen.findByRole('button');
+    await userEvent.hover(button);
+    const tooltip = await screen.findByRole('tooltip');
+    expect(tooltip).toHaveTextContent('Close report issue form');
   });
 });
