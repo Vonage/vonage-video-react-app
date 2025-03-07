a0005db1c29572014c4ba740bfab504628d4c9d0
Misha Behei <misha.behei@vonage.com>
2025-03-07 14:34:16 -0600

    add

--- frontend/src/utils/constants.tsx
+++ frontend/src/utils/constants.tsx
@@ -1,6 +1,6 @@
 /**
  * The base URL determined by the current environment.
- * @returns {string}
+ * @constant {string}
  */
 export const API_URL = window.location.origin.includes('localhost')
   ? 'http://localhost:3345'
@@ -8,7 +8,7 @@ export const API_URL = window.location.origin.includes('localhost')
 
 /**
  * An object representing various states for device access.
- * @returns {object}
+ * @constant {object}
  * @property {string} PENDING - Status when the access to the device is pending.
  * @property {string} ACCEPTED - Status when the access to the device has been granted.
  * @property {string} REJECTED - Status when the access to the device was denied.
@@ -23,62 +23,69 @@ export const DEVICE_ACCESS_STATUS = {
 
 /**
  * A message to alert the user that their microphone is muted.
- * @returns {string}
+ * @constant {string}
  */
 export const MUTED_ALERT_MESSAGE =
   'Are you talking? Your mic is off. Click on the mic to turn it on.';
 
 /**
  * A message to alert the user that their microphone was muted by another participant.
- * @returns {string}
+ * @constant {string}
  */
 export const FORCE_MUTED_ALERT_MESSAGE =
   'You have been muted by another participant. Click on the mic to unmute yourself.';
 
 /**
  * A user-friendly message alerting the user of publishing issues.
- * @returns {string}
+ * @constant {string}
  */
 export const PUBLISHING_BLOCKED_CAPTION =
   "We're having trouble connecting you with others in the meeting room. Please check your network and try again.";
 
 /**
  * The text shadow style used for display purposes.
- * @returns {string}
+ * @constant {string}
  */
 export const TEXT_SHADOW = '[text-shadow:_0_1px_2px_rgb(0_0_0_/_60%)]';
 
 /**
  * The duration in milliseconds for which emojis are displayed.
- * @returns {number}
+ * @constant {number}
  */
 export const EMOJI_DISPLAY_DURATION = 5_000;
 
 /**
  * The maximum number of characters allowed in the Report Issue form for the title input.
- * @returns {number}
+ * @constant {number}
  */
 export const REPORT_TITLE_LIMIT = 100;
 
 /**
  * The maximum number of characters allowed in the Report Issue form for the name input.
- * @returns {number}
+ * @constant {number}
  */
 export const REPORT_NAME_LIMIT = 100;
 
 /**
  * The maximum number of characters allowed in the Report Issue form for the description input.
- * @returns {number}
+ * @constant {number}
  */
 export const REPORT_DESCRIPTION_LIMIT = 1000;
 
+/**
+ * An object representing the browser name and link to download it
+ * @constant {object}
+ * @property {string} browser - The browser name.
+ * @property {string} link - The link to download the said browser.
+ */
 export type SupportedBrowser = {
   browser: string;
   link: string;
 };
+
 /**
  * The browsers supported by Vonage Video API Reference App, and their download links.
- * @returns {SupportedBrowser[]}
+ * @constant {SupportedBrowser[]}
  */
 export const SUPPORTED_BROWSERS = [
   { browser: 'Chrome', link: 'https://www.google.com/chrome/' },
