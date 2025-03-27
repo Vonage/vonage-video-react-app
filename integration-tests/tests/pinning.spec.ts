002cb2654e46eb0dedffeeaccb470088ea1ca6f1
dwivedisachin <sachin.dwivedi@vonage.com>
2025-03-27 19:36:06 +0530

    pinning

--- integration-tests/tests/pinning.spec.ts
+++ integration-tests/tests/pinning.spec.ts
@@ -4,9 +4,12 @@ import { test } from '../fixtures/testWithLogging';
 import { openMeetingRoomWithSettings, waitAndClickFirefox } from './utils';
 
 test.describe('participant pinning', () => {
-  test.skip(({ browserName, isMobile }) => browserName !== 'chromium' || isMobile);
-
-  test('pinned participants should be larger', async ({ page: pageOne, context, browserName }) => {
+  test('pinned participants should be larger', async ({
+    page: pageOne,
+    context,
+    browserName,
+    isMobile,
+  }) => {
     const roomName = crypto.randomBytes(5).toString('hex');
 
     const pageTwo = await context.newPage();
@@ -40,9 +43,15 @@ test.describe('participant pinning', () => {
     await pageThree.waitForSelector('.subscriber', { state: 'visible' });
 
     await expect(await pageThree.locator('.subscriber').getByText('User Two')).toBeVisible();
-
-    // Change view to grid layout
-    await pageThree.getByTestId('ViewSidebarIcon').click();
+    if (isMobile) {
+      await pageThree.getByTestId('MoreVertIcon').click();
+      await pageThree.mouse.move(0, 0); // Moves cursor to top-left corner to hide tooltip
+      await pageThree.getByTestId('ViewSidebarIcon').click();
+      await pageThree.getByTestId('MoreVertIcon').click(); // On mobile after changing the layout the kebab menu is not auto closed
+    } else {
+      // Change view to grid layout
+      await pageThree.getByTestId('ViewSidebarIcon').click();
+    }
     // Give the page some time to render the grid layout
     await pageThree.waitForTimeout(500);
 
