f0812cd6367b9642e5effe1b236b3daaf53faaa5
Misha Behei <misha.behei@vonage.com>
2025-03-31 15:10:49 -0500

    lil comment

--- frontend/src/utils/getCurrentDeviceId/getCurrentDeviceId.tsx
+++ frontend/src/utils/getCurrentDeviceId/getCurrentDeviceId.tsx
@@ -11,6 +11,14 @@ const refreshDevices = () => {
 
 refreshDevices();
 
+/**
+ * Retrieves the stored device ID for a given device type (audio or video)
+ * and checks if it is still connected.
+ * This function is intentionally synchronous to avoid forcing async behavior as it introduces some
+ * inconsistent behavior where it is used. To achieve this, we maintain a cached list of devices that is updated asynchronously.
+ * @param {'audio' | 'video'} kind - the type of the device to check - audio or video.
+ * @returns {string | undefined} deviceId if still connected; undefined otherwise.
+ */
 const getConnectedDeviceId = (kind: 'audioinput' | 'videoinput'): string | undefined => {
   const storageKey = kind === 'audioinput' ? 'audioSource' : 'videoSource';
   const deviceId = window.localStorage.getItem(storageKey);
