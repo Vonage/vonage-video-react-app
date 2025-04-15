bcf882e44f8990c59897c2774281ccad16e94c61
Misha Behei <misha.behei@vonage.com>
2025-04-15 16:37:20 -0500

    isEnabled removed and logged a message to keep sonar happy

--- frontend/src/components/WaitingRoom/VideoContainerButton/VideoContainerButton.tsx
+++ frontend/src/components/WaitingRoom/VideoContainerButton/VideoContainerButton.tsx
@@ -5,7 +5,6 @@ export type VideoContainerButtonProps = {
   onClick: () => void;
   icon: ReactElement;
   sx?: SxProps;
-  isEnabled: boolean;
 };
 
 /**
@@ -16,7 +15,6 @@ export type VideoContainerButtonProps = {
  *  @property {Function} onClick - The on-click handler for the button.
  *  @property {ReactElement} icon - The Icon element for the button.
  *  @property {SxProps} sx - The style properties for the component.
- *  @property {boolean} isEnabled - Whether the button is toggled or not.
  * @returns {ReactElement} The VideoContainerButton component.
  */
 const VideoContainerButton = forwardRef(function VideoContainerButton(
