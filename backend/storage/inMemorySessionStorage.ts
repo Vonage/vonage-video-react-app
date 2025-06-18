e84325d79054e761e1b5400d19bf0424dae148f0
Misha Behei <misha.behei@vonage.com>
2025-06-18 16:58:38 -0500

    refactor session storage

--- backend/storage/inMemorySessionStorage.ts
+++ backend/storage/inMemorySessionStorage.ts
@@ -1,37 +1,70 @@
 import { SessionStorage } from './sessionStorage';
 
+interface SessionData {
+  sessionId: string;
+  captionsId: string | null;
+  captionsUserCount: number;
+}
+
 class InMemorySessionStorage implements SessionStorage {
-  sessions: { [key: string]: string } = {};
+  private sessions: { [key: string]: SessionData } = {};
+
   async getSession(roomName: string): Promise<string | null> {
-    return this.sessions[roomName] || null;
+    return this.sessions[roomName]?.sessionId || null;
   }
 
   async setSession(roomName: string, sessionId: string): Promise<void> {
-    this.sessions[roomName] = sessionId;
+    if (!this.sessions[roomName]) {
+      this.sessions[roomName] = {
+        sessionId,
+        captionsId: null,
+        captionsUserCount: 0,
+      };
+    } else {
+      this.sessions[roomName].sessionId = sessionId;
+    }
   }
 
-  private captionsIds: { [key: string]: string } = {};
-  private captionsUserCount: { [roomName: string]: number } = {};
   async setCaptionId(roomName: string, captionsId: string): Promise<void> {
-    this.captionsIds[roomName] = captionsId;
+    if (!this.sessions[roomName]) {
+      this.sessions[roomName] = {
+        sessionId: '',
+        captionsId,
+        captionsUserCount: 0,
+      };
+    } else {
+      this.sessions[roomName].captionsId = captionsId;
+    }
   }
 
   async getCaptionsId(roomName: string): Promise<string | null> {
-    return this.captionsIds[roomName] || null;
+    return this.sessions[roomName]?.captionsId || null;
   }
 
   async addCaptionsUser(roomName: string): Promise<number> {
-    this.captionsUserCount[roomName] = (this.captionsUserCount[roomName] || 0) + 1;
-    return this.captionsUserCount[roomName];
+    if (!this.sessions[roomName]) {
+      this.sessions[roomName] = {
+        sessionId: '',
+        captionsId: null,
+        captionsUserCount: 1,
+      };
+    } else {
+      this.sessions[roomName].captionsUserCount += 1;
+    }
+    return this.sessions[roomName].captionsUserCount;
   }
 
   async removeCaptionsUser(roomName: string): Promise<number> {
-    this.captionsUserCount[roomName] = (this.captionsUserCount[roomName] || 0) - 1;
-    if (this.captionsUserCount[roomName] < 0) {
-      // Ensure count does not go negative mostly for unit testing purposes
-      this.captionsUserCount[roomName] = 0;
+    if (!this.sessions[roomName]) {
+      return 0;
     }
-    return this.captionsUserCount[roomName];
+
+    this.sessions[roomName].captionsUserCount -= 1;
+    if (this.sessions[roomName].captionsUserCount < 0) {
+      // Ensure count does not go negative mostly for unit testing purposes
+      this.sessions[roomName].captionsUserCount = 0;
+    }
+    return this.sessions[roomName].captionsUserCount;
   }
 }
 export default InMemorySessionStorage;
