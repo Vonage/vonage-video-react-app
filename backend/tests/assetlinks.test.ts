8402d747a72df88a1f87e9ac67baa25ede26d105
Manuel Vera <manuel.veranieto@vonage.com>
2025-08-06 17:49:30 +0200

    VIDSOL-134 add test

---
+++ backend/tests/assetlinks.test.ts
@@ -0,0 +1,49 @@
+import { afterAll, beforeAll, describe, expect, it } from '@jest/globals';
+import request from 'supertest';
+import { Server } from 'http';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+process.env.VIDEO_SERVICE_PROVIDER = 'opentok';
+
+const startServer = (await import('../server')).default;
+const fileName = fileURLToPath(import.meta.url);
+const dirName = path.dirname(fileName);
+
+describe('GET /.well-known/assetlinks.json', () => {
+  let server: Server;
+
+  beforeAll(async () => {
+    server = await startServer();
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  it('returns a 200 status code', async () => {
+    const res = await request(server).get('/.well-known/assetlinks.json');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('returns the correct Content-Type header', async () => {
+    const res = await request(server).get('/.well-known/assetlinks.json');
+    expect(res.headers['content-type']).toMatch(/application\/json/);
+  });
+
+  it('returns valid JSON content', async () => {
+    const res = await request(server).get('/.well-known/assetlinks.json');
+    expect(() => JSON.parse(res.text)).not.toThrow();
+  });
+
+  it('returns the same content as the static assetlinks.json file', async () => {
+    const res = await request(server).get('/.well-known/assetlinks.json');
+    const expectedContent = fs.readFileSync(
+      path.join(dirName, '..', '.well-known', 'assetlinks.json'),
+      'utf8'
+    );
+
+    expect(JSON.parse(res.text)).toEqual(JSON.parse(expectedContent));
+  });
+});
