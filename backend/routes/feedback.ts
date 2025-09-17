97be61b8df6113fb0fa9a36bb5cabed17969d145
Ivan <210490533+VZaphod@users.noreply.github.com>
2025-09-17 16:54:48 +0200

    iOS and Android JIRA component IDs

--- backend/routes/feedback.ts
+++ backend/routes/feedback.ts
@@ -1,14 +1,33 @@
 import { Request, Response, Router } from 'express';
 import getFeedbackService from '../services/getFeedbackService';
+import { FeedbackOrigin } from '../types/feedback';
 
 const feedbackRouter = Router();
 const feedbackService = getFeedbackService();
 
 feedbackRouter.post('/report', async (req: Request, res: Response) => {
+  const { headers } = req;
   const { title, name, issue, attachment } = req.body;
 
   try {
-    const feedbackData = await feedbackService.reportIssue({ title, name, issue, attachment });
+    let origin = FeedbackOrigin.Web;
+    const useragent = headers['User-Agent'] || headers['user-agent'];
+
+    if (useragent != null) {
+      if (useragent.includes('VeraNativeiOS')) {
+        origin = FeedbackOrigin.iOS;
+      } else if (useragent.includes('VeraNativeAndroid')) {
+        origin = FeedbackOrigin.Android;
+      }
+    }
+
+    const feedbackData = await feedbackService.reportIssue({
+      title,
+      name,
+      issue,
+      attachment,
+      origin,
+    });
     if (feedbackData) {
       return res.status(200).json({ feedbackData });
     }
