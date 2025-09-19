310921d8faf07336b4472559bc940c6d9ef050b0
dwivedisachin <sachin.dwivedi@vonage.com>
2025-09-19 12:59:56 +0530

    add a few more languages

--- frontend/src/locales/index.ts
+++ frontend/src/locales/index.ts
@@ -1,27 +1,55 @@
-import EN from './en.json';
-import FR from './fr.json';
 import DE from './de.json';
-import IT from './it.json';
+import EN from './en.json';
 import ES from './es.json';
+import FI from './fi.json';
+import FR from './fr.json';
 import HI from './hi.json';
+import IT from './it.json';
+import JA from './ja.json';
+import KO from './ko.json';
+import NB from './nb.json';
+import PT_BR from './pt-BR.json';
+import PT_PT from './pt-PT.json';
+import SV from './sv.json';
 
 export default {
-  en: {
-    translation: EN,
-  },
-  fr: {
-    translation: FR,
-  },
   de: {
     translation: DE,
   },
-  it: {
-    translation: IT,
+  en: {
+    translation: EN,
   },
   es: {
     translation: ES,
   },
+  fi: {
+    translation: FI,
+  },
+  fr: {
+    translation: FR,
+  },
   hi: {
     translation: HI,
+  },
+  it: {
+    translation: IT,
+  },
+  ja: {
+    translation: JA,
+  },
+  ko: {
+    translation: KO,
+  },
+  nb: {
+    translation: NB,
+  },
+  'pt-BR': {
+    translation: PT_BR,
+  },
+  'pt-PT': {
+    translation: PT_PT,
+  },
+  sv: {
+    translation: SV,
   },
 };
