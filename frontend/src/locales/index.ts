e99c9ebd83dc78b60cfc3ae0340c2292d0193446
ofava <Oscar.Fava@vonage.com>
2025-10-01 12:23:28 +0200

    Remove unsupported languages

--- frontend/src/locales/index.ts
+++ frontend/src/locales/index.ts
@@ -1,55 +1,19 @@
-import DE from './de.json';
 import EN from './en.json';
 import ES from './es.json';
-import FI from './fi.json';
-import FR from './fr.json';
-import HI from './hi.json';
 import IT from './it.json';
-import JA from './ja.json';
-import KO from './ko.json';
-import NB from './nb.json';
-import PT_BR from './pt-BR.json';
-import PT_PT from './pt-PT.json';
-import SV from './sv.json';
+import ES_MX from './es-MX.json';
 
 export default {
-  de: {
-    translation: DE,
-  },
   en: {
     translation: EN,
   },
   es: {
     translation: ES,
   },
-  fi: {
-    translation: FI,
-  },
-  fr: {
-    translation: FR,
-  },
-  hi: {
-    translation: HI,
+  'es-MX': {
+    translation: ES_MX,
   },
   it: {
     translation: IT,
-  },
-  ja: {
-    translation: JA,
-  },
-  ko: {
-    translation: KO,
-  },
-  nb: {
-    translation: NB,
-  },
-  'pt-BR': {
-    translation: PT_BR,
-  },
-  'pt-PT': {
-    translation: PT_PT,
-  },
-  sv: {
-    translation: SV,
   },
 };
