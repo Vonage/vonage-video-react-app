0404b369cd19f785a40b87adfed4f2f290e1f557
Sachin Dwivedi <109963984+dwivedisachin@users.noreply.github.com>
2025-10-01 16:40:52 +0530

    VIDSOL-198: Add languages Json files (#228)
    Co-authored-by: ofava <Oscar.Fava@vonage.com>

--- frontend/src/locales/index.ts
+++ frontend/src/locales/index.ts
@@ -1,7 +1,19 @@
 import EN from './en.json';
+import ES from './es.json';
+import IT from './it.json';
+import ES_MX from './es-MX.json';
 
 export default {
   en: {
     translation: EN,
+  },
+  es: {
+    translation: ES,
+  },
+  'es-MX': {
+    translation: ES_MX,
+  },
+  it: {
+    translation: IT,
   },
 };
