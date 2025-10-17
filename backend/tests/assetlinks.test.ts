979c152f6b9c89be75e3d25c3f5791eedb686ea2
Manuel Vera <manuel.veranieto@vonage.com>
2025-10-17 13:45:51 +0200

    VIDSOL-205 update assetlinks for internal distribution app

--- backend/tests/assetlinks.test.ts
+++ backend/tests/assetlinks.test.ts
@@ -40,9 +40,19 @@ describe('GET /.well-known/assetlinks.json', () => {
           relation: expect.arrayContaining(['delegate_permission/common.handle_all_urls']),
           target: expect.objectContaining({
             namespace: 'android_app',
-            package_name: 'com.vonage.android',
+            package_name: 'com.vonage.android.debug',
             sha256_cert_fingerprints: expect.arrayContaining([
               'A4:26:72:80:DA:75:99:75:ED:D2:32:ED:0A:DC:2C:7C:27:78:6A:8C:9A:37:22:41:23:CF:9E:DB:03:78:FC:6C',
+            ]),
+          }),
+        }),
+        expect.objectContaining({
+          relation: expect.arrayContaining(['delegate_permission/common.handle_all_urls']),
+          target: expect.objectContaining({
+            namespace: 'android_app',
+            package_name: 'com.vonage.android',
+            sha256_cert_fingerprints: expect.arrayContaining([
+              'A5:77:34:82:4C:98:13:CF:88:DF:20:46:2C:B7:7E:33:C0:4C:FC:C6:A1:E4:B3:25:5F:43:49:BA:FE:B5:43:27',
             ]),
           }),
         }),
