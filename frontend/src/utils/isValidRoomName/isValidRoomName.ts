05e787b0c6b562e3f80e3875d3e876b595b637e6
ofava <Oscar.Fava@vonage.com>
2025-11-24 15:57:03 +0100

    Refactor isValidRoomName function to validate input length and update test cases

--- frontend/src/utils/isValidRoomName/isValidRoomName.ts
+++ frontend/src/utils/isValidRoomName/isValidRoomName.ts
@@ -1,4 +1,7 @@
 const isValidRoomName = (name: string) => {
+  if (name.length < 1 || name.length > 100) {
+    return false;
+  }
   // Regular expression to allow letters, numbers, underscores, hyphens, and plus sign only
   const regex = /^[a-z0-9_+-]+$/;
   return regex.test(name);
