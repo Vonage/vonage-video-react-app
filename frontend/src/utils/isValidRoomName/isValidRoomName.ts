a5f9a01b9d3684648d342c61209ea4e31a9624c4
ofava <Oscar.Fava@vonage.com>
2025-11-24 17:39:56 +0100

    Refactor isValidRoomName function for improved readability and maintainability; streamline Box component type definitions

--- frontend/src/utils/isValidRoomName/isValidRoomName.ts
+++ frontend/src/utils/isValidRoomName/isValidRoomName.ts
@@ -1,10 +1,7 @@
 const isValidRoomName = (name: string) => {
-  if (name.length < 1 || name.length > 100) {
-    return false;
-  }
   // Regular expression to allow letters, numbers, underscores, hyphens, and plus sign only
   const regex = /^[a-z0-9_+-]+$/;
-  return regex.test(name);
+  return regex.test(name) && name.length >= 1 && name.length <= 100;
 };
 
 export default isValidRoomName;
