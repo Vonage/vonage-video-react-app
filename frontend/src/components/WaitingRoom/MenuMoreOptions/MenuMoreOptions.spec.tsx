dc04169534da16b187bb09ad140d2409e19f9179
OscarFava <Oscar.Fava@vonage.com>
2025-12-11 13:11:35 +0100

    Refactor MenuMoreOptions tests to consistently include appConfigOptions for background effects

--- frontend/src/components/WaitingRoom/MenuMoreOptions/MenuMoreOptions.spec.tsx
+++ frontend/src/components/WaitingRoom/MenuMoreOptions/MenuMoreOptions.spec.tsx
@@ -6,35 +6,78 @@ import backgroundEffectsDialog$ from '@Context/BackgroundEffectsDialog';
 import { AppConfigProviderWrapperOptions, makeAppConfigProviderWrapper } from '@test/providers';
 import MenuMoreOptions from './MenuMoreOptions';
 
+const { mockHasMediaProcessorSupport } = vi.hoisted(() => {
+  return {
+    mockHasMediaProcessorSupport: vi.fn().mockReturnValue(true),
+  };
+});
+
+vi.mock('@vonage/client-sdk-video', () => ({
+  hasMediaProcessorSupport: mockHasMediaProcessorSupport,
+}));
+
 describe('MenuMoreOptions', () => {
   const mockOnClose = vi.fn();
   const mockAnchorEl = document.createElement('button');
 
   beforeEach(() => {
     mockOnClose.mockClear();
+    mockHasMediaProcessorSupport.mockReturnValue(true);
   });
 
   it('should render when open is true', () => {
-    render(<MenuMoreOptions onClose={mockOnClose} open anchorEl={mockAnchorEl} />);
+    render(<MenuMoreOptions onClose={mockOnClose} open anchorEl={mockAnchorEl} />, {
+      appConfigOptions: {
+        value: {
+          videoSettings: {
+            allowBackgroundEffects: true,
+          },
+        },
+      },
+    });
 
     expect(screen.getByTestId('menu-more-options')).toBeInTheDocument();
   });
 
   it('should not render menu items when open is false', () => {
-    render(<MenuMoreOptions onClose={mockOnClose} open={false} anchorEl={mockAnchorEl} />);
+    render(<MenuMoreOptions onClose={mockOnClose} open={false} anchorEl={mockAnchorEl} />, {
+      appConfigOptions: {
+        value: {
+          videoSettings: {
+            allowBackgroundEffects: true,
+          },
+        },
+      },
+    });
 
     expect(screen.queryByText(/background effects/i)).not.toBeInTheDocument();
   });
 
   it('should display background effects option', () => {
-    render(<MenuMoreOptions onClose={mockOnClose} open anchorEl={mockAnchorEl} />);
+    render(<MenuMoreOptions onClose={mockOnClose} open anchorEl={mockAnchorEl} />, {
+      appConfigOptions: {
+        value: {
+          videoSettings: {
+            allowBackgroundEffects: true,
+          },
+        },
+      },
+    });
 
     expect(screen.getByText(/background effects/i)).toBeInTheDocument();
   });
 
   it('should call onClose when clicking on background effects option', async () => {
     const user = userEvent.setup();
-    render(<MenuMoreOptions onClose={mockOnClose} open anchorEl={mockAnchorEl} />);
+    render(<MenuMoreOptions onClose={mockOnClose} open anchorEl={mockAnchorEl} />, {
+      appConfigOptions: {
+        value: {
+          videoSettings: {
+            allowBackgroundEffects: true,
+          },
+        },
+      },
+    });
 
     const menuItem = screen.getByText(/background effects/i);
     await user.click(menuItem);
@@ -43,7 +86,15 @@ describe('MenuMoreOptions', () => {
   });
 
   it('should display icon for background effects', () => {
-    render(<MenuMoreOptions onClose={mockOnClose} open anchorEl={mockAnchorEl} />);
+    render(<MenuMoreOptions onClose={mockOnClose} open anchorEl={mockAnchorEl} />, {
+      appConfigOptions: {
+        value: {
+          videoSettings: {
+            allowBackgroundEffects: true,
+          },
+        },
+      },
+    });
 
     expect(screen.getByTestId('vivid-icon-gallery-line')).toBeInTheDocument();
   });
