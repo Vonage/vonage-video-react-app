16338a831120550164d97ff846a6478d7696a09f
OscarFava <Oscar.Fava@vonage.com>
2025-12-22 11:21:16 +0100

    Add global setup function for Playwright tests

---
+++ integration-tests/globalSetup.js
@@ -0,0 +1,7 @@
+/**
+ * Playwright global setup function.
+ * This runs once before all tests.
+ */
+export default async function globalSetup() {
+  // Global setup logic can be added here if needed
+}
