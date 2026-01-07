c67329922c1a0b7ed652c4fe6009e9a756f3c70a
Johnny Esteban Quesada <johnny-qc@hotmail.com>
2026-01-07 12:27:26 +0100

    johnny/VIDSOL-323-fixing dev deploy no delete files needed
    pending prod

--- vcrBuild.dev.sh
+++ vcrBuild.dev.sh
@@ -1,14 +1,38 @@
-#!/bin/bash
-set -e
+# run install skipping post install script which requires husky
+yarn install --production=false --ignore-scripts --frozen-lockfile
+yarn build
 
-# check for pending changes to commit
-if [ -n "$(git status --porcelain)" ]; then
-  echo "You have uncommitted changes. Please commit or stash them before running this script."
+# copy config file to the build output
+if [ -f ./config.json ]; then
+  cp ./config.json ./backend/dist/config.json
+else
+  cp ./config.example.json ./backend/dist/config.json
+fi
+
+# copy env file to the build output (FAIL if missing)
+if [ -f ./backend/.env ]; then
+  cp ./backend/.env ./backend/dist/.env
+else
+  echo "❌ ERROR: ./backend/.env file not found"
   exit 1
 fi
 
-# execute the original script
-sh ./vcrBuild.sh
+# copy VCR manifest to the build output (FAIL if missing)
+if [ -f ./vcr-dev.yml ]; then
+  cp ./vcr-dev.yml ./backend/dist/vcr-dev.yml
+else
+  echo "❌ ERROR: ./vcr-dev.yml not found"
+  exit 1
+fi
 
-# restore repo state
-git restore .
+# copy vcr-dev.yml manifest to the build output (FAIL if missing)
+if [ -f ./vcr-dev.yml ]; then
+  cp ./vcr-dev.yml ./backend/dist/vcr-dev.yml
+else
+  echo "❌ ERROR: ./vcr-dev.yml not found"
+  exit 1
+fi
+
+echo ""
+echo "Successfully prepared backend/dist:"
+find backend/dist -print
