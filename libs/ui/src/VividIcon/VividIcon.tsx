a0ab91da999221683350c6329a7dfff835347d04
Hossein Movahed <hr.movahed6@gmail.com>
2026-05-05 13:16:08 +0200

    fix: remove sx form vividIcon

--- libs/ui/src/VividIcon/VividIcon.tsx
+++ libs/ui/src/VividIcon/VividIcon.tsx
@@ -1,48 +1,23 @@
 import { hasMediaProcessorSupport } from '@vonage/client-sdk-video';
-import type { SxProps, Theme } from '@mui/material';
 import { createElement } from 'react';
 import type { CSSProperties, ReactElement } from 'react';
 
 export type VividIconProps = Record<string, unknown> & {
   name: string;
   customSize: -6 | -5 | -4 | -3 | -2 | -1 | 0 | 1 | 2 | 3 | 4 | 5;
-  sx?: SxProps<Theme>;
   style?: CSSProperties;
 };
 
-const VividIcon = ({ name, customSize, sx, style, ...props }: VividIconProps): ReactElement => {
-  const convertedStyle = convertSxToStyle({ sx, style });
-
+const VividIcon = ({ name, customSize, style, ...props }: VividIconProps): ReactElement => {
   return createElement('vwc-icon', {
     ref: captureRefComponent,
     size: customSize,
     name,
     'data-testid': `vivid-icon-${name}`,
-    style: convertedStyle,
+    style,
     ...props,
   });
 };
-
-function convertSxToStyle({
-  sx,
-  style,
-}: {
-  sx?: SxProps<Theme>;
-  style?: CSSProperties;
-}): CSSProperties | undefined {
-  if (!sx && !style) return undefined;
-
-  let convertedSx: CSSProperties = {};
-
-  if (sx && typeof sx === 'object' && !Array.isArray(sx)) {
-    convertedSx = sx as CSSProperties;
-  }
-
-  return {
-    ...convertedSx,
-    ...style,
-  };
-}
 
 function captureRefComponent(element: HTMLElement | null) {
   if (!element || hasMediaProcessorSupport()) return;
