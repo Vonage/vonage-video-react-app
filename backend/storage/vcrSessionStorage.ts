f2868df112a48f4822a59fa23e40babeddc543c9
Ivan <210490533+VZaphod@users.noreply.github.com>
2026-06-12 11:26:35 +0200

    Added prefix and suffix to redis keys

--- backend/storage/vcrSessionStorage.ts
+++ backend/storage/vcrSessionStorage.ts
@@ -11,7 +11,7 @@ enum StorageResource {
 }
 
 function makeKey(resource: StorageResource, id: string): string {
-  return `${resource}:${id}`;
+  return `A${resource}:${id}A`;
 }
 
 class VcrSessionStorage implements SessionStorage {
