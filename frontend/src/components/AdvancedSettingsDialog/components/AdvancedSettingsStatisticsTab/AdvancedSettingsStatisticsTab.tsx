945a51947b375c717b168ed8dd542dbf452f551e
OscarFava <Oscar.Fava@vonage.com>
2026-07-30 11:41:56 +0200

    Fix

--- frontend/src/components/AdvancedSettingsDialog/components/AdvancedSettingsStatisticsTab/AdvancedSettingsStatisticsTab.tsx
+++ frontend/src/components/AdvancedSettingsDialog/components/AdvancedSettingsStatisticsTab/AdvancedSettingsStatisticsTab.tsx
@@ -44,9 +44,10 @@ const AdvancedSettingsStatisticsTab = (): ReactElement => {
           </h4>
         )}
 
-        {subscriberWrappers.map(({ subscriber, id }, index) => {
-          subscriber.id = id;
-          return <SubscriberStatistics subscriber={subscriber} key={id ?? index} />;
+        {subscriberWrappers.map((subscriber, index) => {
+          return (
+            <SubscriberStatistics subscriberWrapper={subscriber} key={subscriber.id ?? index} />
+          );
         })}
       </div>
     </div>
